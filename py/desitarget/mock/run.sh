305ba3854717092bf4a411ee957eadb13904e160
michaelJwilson <mjwilson@lbl.gov>
2020-02-09 18:01:21 -0800

    Clened up IO.

--- py/desitarget/mock/run.sh
+++ py/desitarget/mock/run.sh
@@ -1,4 +1,4 @@
 export PATH=/global/homes/m/mjwilson/desi/E2E/desitarget/bin/:$PATH
 export PYTHONPATH=/global/homes/m/mjwilson/desi/E2E/desitarget/py/:/global/homes/m/mjwilson/desi/E2E/desitarget/:$PYTHONPATH
 
-select_mock_targets -c /global/homes/m/mjwilson/desi/E2E/desitarget/py/desitarget/mock/data/select-mock-targets.yaml --output_dir /global/homes/m/mjwilson/desi/E2E/desitarget/scratch -n 4 --survey main --healpixels 1 --no-spectra --nside 256 --no-spectra --overwrite --verbose
+select_mock_targets -c /global/homes/m/mjwilson/desi/E2E/desitarget/py/desitarget/mock/data/select-mock-targets.yaml --output_dir /global/cscratch1/sd/mjwilson/desi/E2E/desitarget/ -n 4 --survey main --healpixels 1 --no-spectra --nside 64 --no-spectra --overwrite --verbose --sampling release
